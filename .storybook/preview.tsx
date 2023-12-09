1eda9880ed31d235880af00e7a9ed9efd45804f5
Edward Arechwa <e.arechwa@nebucode.com>
2023-12-09 01:04:31 +0100

    ft: add next-intl to storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,21 @@
 import type { Preview } from "@storybook/react";
+import { NextIntlClientProvider } from "next-intl";
+import React from "react";
+
 import "../src/styles/index.css";
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <NextIntlClientProvider
+        timeZone="Europe/Warsaw"
+        locale={"en"}
+        messages={{}}
+      >
+        <Story />
+      </NextIntlClientProvider>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
