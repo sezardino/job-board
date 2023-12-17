b35db1929b10025d26bcb0860ce58e452896c6c1
Edward Arechwa <e.arechwa@nebucode.com>
2023-12-17 16:06:59 +0100

    ft: function for upload cv

--- src/services/server/modules/files/files.service.ts
+++ src/services/server/modules/files/files.service.ts
@@ -2,14 +2,29 @@ import { uploadFile } from "@/libs/firebase/storage";
 import { AbstractService } from "@/services/server/helpers";
 
 export class FilesService extends AbstractService {
-  async uploadImage(file: File) {
-    const imageData = await uploadFile("images", file);
+  async uploadImage(file: File, companyId: string) {
+    const imageData = await uploadFile(`${companyId}/images`, file);
 
     if (!imageData) {
       return Promise.reject("Error when try to upload image");
     }
 
     return this.prismaService.image.create({
+      data: {
+        name: imageData.fileName,
+        url: imageData.publicUrl,
+      },
+    });
+  }
+
+  async uploadCV(file: File, companyId: string) {
+    const imageData = await uploadFile(`${companyId}/cv`, file);
+
+    if (!imageData) {
+      return Promise.reject("Error when try to upload CV");
+    }
+
+    return this.prismaService.curriculumVitae.create({
       data: {
         name: imageData.fileName,
         url: imageData.publicUrl,
