26947ca2db5061b57a226ef60080377672501080
Edward <e.arechwa@gmail.com>
2024-04-01 12:19:53 +0200

    Refactor CategoryCard and BoardLayout components

--- src/components/UI/CategoryCard/CategoryCard.tsx
+++ src/components/UI/CategoryCard/CategoryCard.tsx
@@ -1,56 +1,40 @@
 import { Button } from "@/components/base/Button/Button";
 import { Icon } from "@/components/base/Icon/Icon";
-import { Typography } from "@/components/base/Typography/Typography";
 import { Tooltip } from "@nextui-org/react";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { type ComponentPropsWithoutRef, type FC } from "react";
-import { twMerge } from "tailwind-merge";
 import { categoriesIconMapping } from "./icons";
-
-import styles from "./CategoryCard.module.scss";
 
 type Props = {
   name: string;
   prefix?: string;
   isActive: boolean;
-  labelType?: "tooltip" | "label";
 };
 
-type OmittedProps = Omit<
-  ComponentPropsWithoutRef<"a">,
+export type CategoryCardProps = Omit<
+  ComponentPropsWithoutRef<"button">,
   "onFocus" | "onBlur" | "color"
->;
-
-export type CategoryCardProps = OmittedProps & Props;
+> &
+  Props;
 
 export const CategoryCard: FC<CategoryCardProps> = (props) => {
-  const {
-    labelType = "tooltip",
-    name,
-    prefix,
-    isActive,
-    className,
-    ...rest
-  } = props;
+  const { name, prefix, isActive, className, ...rest } = props;
   const categoryT = useTranslations("entity.categories");
 
   const iconName = categoriesIconMapping[name] || "HiQuestionMarkCircle";
 
   return (
-    <Tooltip isDisabled={labelType !== "tooltip"} content={categoryT(name)}>
-      <Link
+    <Tooltip content={categoryT(name)}>
+      <Button
         {...rest}
+        isIconOnly
+        as={Link}
         href={prefix ? prefix + "/" + name : name}
-        className={twMerge(styles.element)}
+        color={isActive ? "primary" : undefined}
       >
-        <Button as="div" isIconOnly color={isActive ? "primary" : undefined}>
-          <Icon name={iconName} size={24} />
-        </Button>
-        {labelType === "label" && (
-          <Typography tag="span">{categoryT(name)}</Typography>
-        )}
-      </Link>
+        <Icon name={iconName} size={24} />
+      </Button>
     </Tooltip>
   );
 };
--- src/components/layout/Board/BoardLayout.tsx
+++ src/components/layout/Board/BoardLayout.tsx
@@ -8,7 +8,7 @@ import { type ComponentPropsWithoutRef, type FC } from "react";
 
 import styles from "./BoardLayout.module.scss";
 
-import { SearchForm } from "@/components/base/SearchForm/SearchForm";
+import { Skeleton } from "@nextui-org/react";
 import { twMerge } from "tailwind-merge";
 
 type Props = {
@@ -39,32 +39,24 @@ export const BoardLayoutWrapper: FC<BoardLayoutProps> = (props) => {
   return (
     <div {...rest} className={twMerge(styles.element, className)}>
       <LandingNavbar user={user} onSignOutClick={onSignOutClick}>
-        <div className={styles.wrapper}>
-          <ul className={styles.list}>
-            {/* {isActiveCategoriesLoading &&
-              new Array(10)
-                .fill(null)
-                .map((_, index) => (
-                  <Skeleton key={index} className={styles.skeleton} />
-                ))} */}
+        <ul className={styles.list}>
+          {isActiveCategoriesLoading &&
+            new Array(10)
+              .fill(null)
+              .map((_, index) => (
+                <Skeleton key={index} className={styles.skeleton} />
+              ))}
 
-            {activeCategories?.data.map((category) => (
-              <li key={category.id}>
-                <CategoryCard
-                  name={category.name}
-                  prefix={activeCategory ? undefined : industry}
-                  isActive={activeCategory === category.name}
-                />
-              </li>
-            ))}
-          </ul>
-
-          <SearchForm
-            onSearch={() => undefined}
-            type="submit"
-            className="max-w-80"
-          />
-        </div>
+          {activeCategories?.data.map((category) => (
+            <li key={category.id}>
+              <CategoryCard
+                name={category.name}
+                prefix={activeCategory ? undefined : industry}
+                isActive={activeCategory === category.name}
+              />
+            </li>
+          ))}
+        </ul>
       </LandingNavbar>
       <main className="container">{children}</main>
     </div>
