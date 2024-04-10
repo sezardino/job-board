9b6f72507c72fdce571c9aaa59cc145284b4b51a
Edward <e.arechwa@gmail.com>
2024-04-10 22:10:22 +0200

    Add react-pdf package and update localization for PDF viewer modal

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- src/components/modules/application/ApplicationPreviewModal/ApplicationPreviewModal.tsx
+++ src/components/modules/application/ApplicationPreviewModal/ApplicationPreviewModal.tsx
@@ -7,8 +7,9 @@ import {
   CardHeader,
 } from "@nextui-org/react";
 import dayjs from "dayjs";
-import { FC, useId } from "react";
+import { FC, useId, useState } from "react";
 
+import { PDFViewerModal } from "@/components/UI/PDFViewerModal/PDFViewerModal";
 import { TitleDescription } from "@/components/UI/TitleDescription/TitleDescription";
 import { Badge } from "@/components/base/Badge/Badge";
 import { Button } from "@/components/base/Button/Button";
@@ -40,173 +41,186 @@ export const ApplicationPreviewModal: FC<ApplicationPreviewModalProps> = (
   const { application, isOpen, onClose, onAfterClose, onCreateNote } = props;
   const t = useTranslations("components.application-preview-modal");
   const noteFormId = useId();
+  const [isCvModalOpen, setIsCvModalOpen] = useState(false);
 
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-      size="5xl"
-      onAfterClose={onAfterClose}
-    >
-      <Modal.Header className={styles.header}>
-        <div className={styles.headerWrapper}>
-          <TitleDescription
-            titleLevel="h2"
-            titleStyling="lg"
-            title={t("title")}
-            description={t("description")}
-          />
-
-          <Badge color="warning">{application.data?.status}</Badge>
-        </div>
-
-        <div>
-          <Typography tag="p" styling="sm">
-            {t("applied-at", {
-              value: dayjs(application.data?.createdAt).format(
-                APPLICATION_DATE_FORMAT
-              ),
-            })}
-          </Typography>
-          <Typography tag="p" styling="sm">
-            {t("updated-at", {
-              value: dayjs(application.data?.updatedAt).format(
-                APPLICATION_DATE_FORMAT
-              ),
-            })}
-          </Typography>
-        </div>
-      </Modal.Header>
-
-      <Modal.Body className={styles.body}>
-        <section>
-          <Typography tag="h3" styling="md">
-            {t("agreements.title")}
-          </Typography>
-
-          <Grid gap={4}>
-            <Checkbox
-              isReadOnly
-              checked={application.data?.dataProcessing}
-              label={t("agreements.data-processing")}
+    <>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        size="5xl"
+        onAfterClose={onAfterClose}
+      >
+        <Modal.Header className={styles.header}>
+          <div className={styles.headerWrapper}>
+            <TitleDescription
+              titleLevel="h2"
+              titleStyling="lg"
+              title={t("title")}
+              description={t("description")}
             />
-            <Checkbox
-              isReadOnly
-              checked={application.data?.futureRecruitment}
-              label={t("agreements.future-recruitment")}
-            />
-          </Grid>
-        </section>
 
-        {application.data?.rejectedReason && (
-          <Card>
-            <CardHeader>
-              <Typography tag="h3" styling="md">
-                {t("rejected-reason")}
-              </Typography>
-            </CardHeader>
-            <CardBody>
-              <Typography tag="p" styling="sm">
-                {application.data?.rejectedReason}
-              </Typography>
-            </CardBody>
-          </Card>
-        )}
+            <Badge color="warning">{application.data?.status}</Badge>
+          </div>
 
-        <Accordion
-          defaultSelectedKeys={["candidate"]}
-          isCompact
-          variant="bordered"
-          selectionMode="multiple"
-        >
-          <AccordionItem key="candidate" title={t("candidate.title")}>
-            <Grid gap={4} className={styles.wrapper}>
-              <Input
+          <div>
+            <Typography tag="p" styling="sm">
+              {t("applied-at", {
+                value: dayjs(application.data?.createdAt).format(
+                  APPLICATION_DATE_FORMAT
+                ),
+              })}
+            </Typography>
+            <Typography tag="p" styling="sm">
+              {t("updated-at", {
+                value: dayjs(application.data?.updatedAt).format(
+                  APPLICATION_DATE_FORMAT
+                ),
+              })}
+            </Typography>
+          </div>
+        </Modal.Header>
+
+        <Modal.Body className={styles.body}>
+          <section>
+            <Typography tag="h3" styling="md">
+              {t("agreements.title")}
+            </Typography>
+
+            <Grid gap={4}>
+              <Checkbox
                 isReadOnly
-                isDisabled
-                label={t("candidate.name")}
-                value={application.data?.name}
+                checked={application.data?.dataProcessing}
+                label={t("agreements.data-processing")}
               />
-              <Input
+              <Checkbox
                 isReadOnly
-                isDisabled
-                label={t("candidate.email")}
-                value={application.data?.email}
+                checked={application.data?.futureRecruitment}
+                label={t("agreements.future-recruitment")}
               />
-              <BaseTextarea
-                isDisabled
-                isReadOnly
-                label={t("candidate.message")}
-                disableAnimation
-                value={application.data?.message}
-              />
-              <Button
-                size="lg"
-                variant="bordered"
-                color="primary"
-                className={styles.cv}
-              >
-                <Icon name="HiDocument" size={40} />
-                {t("candidate.cv")}
-              </Button>
             </Grid>
-          </AccordionItem>
+          </section>
 
-          <AccordionItem
-            key="notes"
-            title={
-              <>
-                <Typography tag="span" styling="sm">
-                  {t("notes.title")}
-                </Typography>
-                <Badge size="sm">{application.data?.notes.length}</Badge>
-              </>
-            }
-            textValue={t("notes.title")}
-          >
-            <Grid gap={4} className={styles.wrapper}>
-              {application.data?.notes.length === 0 && (
-                <Typography tag="p" styling="sm">
-                  {t("notes.empty")}
-                </Typography>
-              )}
-
-              {application.data?.notes.length && (
-                <Grid tag="ul" gap={1}>
-                  {application.data?.notes.map((note) => (
-                    <NoteCard
-                      as="li"
-                      key={note.id}
-                      author={note.author}
-                      content={note.content}
-                      createdAt={note.createdAt}
-                      name={note.name}
-                      shadow="none"
-                    />
-                  ))}
-                </Grid>
-              )}
-
-              <Grid gap={4}>
+          {application.data?.rejectedReason && (
+            <Card>
+              <CardHeader>
                 <Typography tag="h3" styling="md">
-                  {t("notes.form")}
+                  {t("rejected-reason")}
                 </Typography>
+              </CardHeader>
+              <CardBody>
+                <Typography tag="p" styling="sm">
+                  {application.data?.rejectedReason}
+                </Typography>
+              </CardBody>
+            </Card>
+          )}
 
-                <NoteForm id={noteFormId} onFormSubmit={onCreateNote} />
-
-                <Button form={noteFormId} type="submit">
-                  {t("notes.add")}
+          <Accordion
+            defaultSelectedKeys={["candidate"]}
+            isCompact
+            variant="bordered"
+            selectionMode="multiple"
+          >
+            <AccordionItem key="candidate" title={t("candidate.title")}>
+              <Grid gap={4} className={styles.wrapper}>
+                <Input
+                  isReadOnly
+                  isDisabled
+                  label={t("candidate.name")}
+                  value={application.data?.name}
+                />
+                <Input
+                  isReadOnly
+                  isDisabled
+                  label={t("candidate.email")}
+                  value={application.data?.email}
+                />
+                <BaseTextarea
+                  isDisabled
+                  isReadOnly
+                  label={t("candidate.message")}
+                  disableAnimation
+                  value={application.data?.message}
+                />
+                <Button
+                  size="lg"
+                  variant="bordered"
+                  color="primary"
+                  className={styles.cv}
+                >
+                  <Icon
+                    name="HiDocument"
+                    size={40}
+                    onClick={() => setIsCvModalOpen(true)}
+                  />
+                  {t("candidate.cv")}
                 </Button>
               </Grid>
-            </Grid>
-          </AccordionItem>
-        </Accordion>
-      </Modal.Body>
-      <Modal.Footer className={styles.footer}>
-        <Button onClick={onClose} variant="bordered">
-          {t("close")}
-        </Button>
-      </Modal.Footer>
-    </Modal>
+            </AccordionItem>
+
+            <AccordionItem
+              key="notes"
+              title={
+                <>
+                  <Typography tag="span" styling="sm">
+                    {t("notes.title")}
+                  </Typography>
+                  <Badge size="sm">{application.data?.notes.length}</Badge>
+                </>
+              }
+              textValue={t("notes.title")}
+            >
+              <Grid gap={4} className={styles.wrapper}>
+                {application.data?.notes.length === 0 && (
+                  <Typography tag="p" styling="sm">
+                    {t("notes.empty")}
+                  </Typography>
+                )}
+
+                {application.data?.notes.length && (
+                  <Grid tag="ul" gap={1}>
+                    {application.data?.notes.map((note) => (
+                      <NoteCard
+                        as="li"
+                        key={note.id}
+                        author={note.author}
+                        content={note.content}
+                        createdAt={note.createdAt}
+                        name={note.name}
+                        shadow="none"
+                      />
+                    ))}
+                  </Grid>
+                )}
+
+                <Grid gap={4}>
+                  <Typography tag="h3" styling="md">
+                    {t("notes.form")}
+                  </Typography>
+
+                  <NoteForm id={noteFormId} onFormSubmit={onCreateNote} />
+
+                  <Button form={noteFormId} type="submit">
+                    {t("notes.add")}
+                  </Button>
+                </Grid>
+              </Grid>
+            </AccordionItem>
+          </Accordion>
+        </Modal.Body>
+        <Modal.Footer className={styles.footer}>
+          <Button onClick={onClose} variant="bordered">
+            {t("close")}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <PDFViewerModal
+        isOpen={isCvModalOpen}
+        onClose={() => setIsCvModalOpen(false)}
+        file={application.data?.curriculumVitae.url}
+      />
+    </>
   );
 };
