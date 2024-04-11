84d38db1a3ece964a2e7e78de884213ebdebbc96
Edward <e.arechwa@gmail.com>
2024-04-11 23:56:52 +0200

    Add CompaniesTable component and update ManageCompaniesTemplate

---
+++ src/components/modules/admin/CompaniesTable.tsx
@@ -0,0 +1,58 @@
+import {
+  TableWidget,
+  TableWidgetProps,
+} from "@/components/UI/TableWidget/TableWidget";
+import { UserInfo } from "@/components/UI/UserInfo/UserInfo";
+import { AdminCompaniesResponse } from "@/services/bll/modules/companies/schema";
+import { createColumnHelper } from "@tanstack/react-table";
+import { useTranslations } from "next-intl";
+import { useMemo, type FC } from "react";
+
+type Company = AdminCompaniesResponse["data"][number];
+
+export type CompaniesTableProps = Omit<TableWidgetProps<Company>, "columns">;
+
+const CH = createColumnHelper<Company>();
+
+export const CompaniesTable: FC<CompaniesTableProps> = (props) => {
+  const t = useTranslations("page.admin.manage-companies");
+
+  const columns = useMemo(
+    () => [
+      CH.accessor("name", {
+        enableSorting: false,
+        header: t("table.head.name"),
+      }),
+      CH.accessor("owner", {
+        enableSorting: false,
+        header: t("table.head.owner"),
+        cell: (row) => (
+          <UserInfo
+            name={row.getValue().name}
+            email={row.getValue().email}
+            avatar={row.getValue().avatar?.url}
+          />
+        ),
+      }),
+      CH.accessor("_count.members", {
+        enableSorting: false,
+        header: t("table.head.members"),
+      }),
+      CH.accessor("_count.offers", {
+        enableSorting: false,
+        header: t("table.head.offers"),
+      }),
+    ],
+    [t]
+  );
+
+  return (
+    <TableWidget
+      {...props}
+      // @ts-ignore
+      columns={columns}
+      noDataMessage={t("table.no-data")}
+      className="mt-4"
+    />
+  );
+};
