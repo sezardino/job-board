abb0f1f9afa14dad80c1ddc1fd4d9e28114b767a
Edward <e.arechwa@gmail.com>
2024-05-04 16:30:43 +0200

    fix: login functionality not work correctly

--- src/hooks/react-query/mutation/auth/login.ts
+++ src/hooks/react-query/mutation/auth/login.ts
@@ -5,11 +5,14 @@ import { CURRENT_USER_PROFILE_QUERY_KEY } from "../../query/users/current-user";
 
 export const useLoginMutation = () =>
   useMutationHelper({
-    mutationFn: (values: AuthFormValues) =>
-      signIn("credentials", {
+    mutationFn: async (values: AuthFormValues) => {
+      const response = await signIn("credentials", {
         ...values,
         redirect: false,
-      }),
+      });
+
+      if (!response?.ok) throw new Error();
+    },
     getQueriesToInvalidate: () => [[CURRENT_USER_PROFILE_QUERY_KEY]],
     successTranslationKey: "login.success",
     errorTranslationKey: "login.error",
